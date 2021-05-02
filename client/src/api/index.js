3bc2c882ef0470ae7d07363d2b491c3ba374e7d2
Marissa Gvozdenovich <66572562+mgvoz@users.noreply.github.com>
2021-05-02 13:44:49 -0400

    Create index.js

---
+++ client/src/api/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'http://localhost:5000' });
+API.interceptors.request.use((req) => {
+	if (localStorage.getItem('profile')) {
+		req.headers.Authorization = `Bearer ${
+			JSON.parse(localStorage.getItem('profile')).token
+		}`;
+	}
+	return req;
+});
+
+export const signIn = (userData) => API.post('/user/signin', userData);
+export const signUp = (userData) => API.post('/user/signup', userData);
+
+export const getRubrics = () => API.get('/rubrics');
+
+export const createRubric = (newRubric) => API.post('/rubrics', newRubric);
+
+export const updateRubric = (id, updatedRubric) =>
+	API.patch(`/rubrics/${id}`, updatedRubric);
+
+export const deleteRubric = (id) => API.delete(`/rubrics/${id}`);
+
+export const getFiles = () => API.get('/files');
