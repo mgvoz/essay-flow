39a8e1570c6d8e6f75724e53fdc6d8a94073b0fc
Marissa Gvozdenovich <66572562+mgvoz@users.noreply.github.com>
2021-05-02 13:49:45 -0400

    Delete src directory

--- src/constants/actionTypes.js
+++
@@ -1,9 +0,0 @@
-export const FETCH_ALL = 'FETCH_ALL';
-export const CREATE_RUBRIC = 'CREATE_RUBRIC';
-export const UPDATE_RUBRIC = 'UPDATE_RUBRIC';
-export const DELETE_RUBRIC = 'DELETE_RUBRIC';
-
-export const FETCH_ALL_FILES = 'FETCH_ALL_FILES';
-
-export const AUTH = 'AUTH';
-export const LOGOUT = 'LOGOUT';
