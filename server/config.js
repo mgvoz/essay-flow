96185221ccbdf2e869f3930b9c39ee567d44e9d2
Marissa Gvozdenovich <66572562+mgvoz@users.noreply.github.com>
2021-05-10 16:29:56 -0400

    hide credentials

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 export const CONNECTION_URL =
-	'mongodb+srv://MarissaG:Hudson91@essay-flow.2vicy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+	'mongodb+srv://***:***@essay-flow.2vicy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 export default CONNECTION_URL;
