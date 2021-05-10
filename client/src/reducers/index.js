3ca154fcef1bb9b03428fbbd68bee02ece8918c7
Marissa Gvozdenovich <66572562+mgvoz@users.noreply.github.com>
2021-05-10 16:40:59 -0400

    added filedata

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import rubrics from './rubrics';
 import auth from './auth';
 import files from './files';
+import filedata from './filedata';
 
 export default combineReducers({
 	rubrics,
