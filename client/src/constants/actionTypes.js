576bcd483bd52b753f01ec75a4fbf8fe699de1c9
Marissa Gvozdenovich <66572562+mgvoz@users.noreply.github.com>
2021-05-10 16:39:34 -0400

    add constants for filedata

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -5,5 +5,11 @@ export const DELETE_RUBRIC = 'DELETE_RUBRIC';
 
 export const FETCH_ALL_FILES = 'FETCH_ALL_FILES';
 
+export const FETCH_ALL_FILEDATA = 'FETCH_ALL_FILEDATA';
+export const CREATE_FILEDATA = 'CREATE_FILEDATA';
+export const UPDATE_FILEDATA = 'UPDATE_FILEDATA';
+export const DELETE_FILEDATA = 'DELETE_FILEDATA';
+
 export const AUTH = 'AUTH';
 export const LOGOUT = 'LOGOUT';
+
