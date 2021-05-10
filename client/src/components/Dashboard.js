3ba8e5665e0f8ffcabb68e76224e4d64ca3939b3
Marissa Gvozdenovich <66572562+mgvoz@users.noreply.github.com>
2021-05-10 16:51:51 -0400

    final edits, update functionality for filedata

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Dashboard({ rubrics, files }) {
+function Dashboard({ rubrics, files, fileData }) {
 	//set variables
 	const user = JSON.parse(localStorage.getItem('profile'));
 
@@ -20,36 +20,52 @@ function Dashboard({ rubrics, files }) {
 
 	const thisUsersFiles = flatArr.filter(
 		(file) =>
-			user?.result?.googleId === file?.metadata.userId ||
-			user?.result?._id === file?.metadata.userId,
+			user?.result?.googleId === file?.metadata ||
+			user?.result?._id === file?.metadata,
+	);
+
+	//get only filedata for signed in user
+	const thisUsersFileData = fileData.filter(
+		(data) =>
+			user?.result?.googleId === data?.userId ||
+			user?.result?._id === data?.userId,
 	);
 
 	//get # of essays graded
-	const essaysGraded = thisUsersFiles.filter(
-		(f) => f?.metadata?.currentGrade !== 'Not yet graded.',
+	const essaysGraded = thisUsersFileData.filter(
+		(f) => f?.currentGrade !== 'Not yet graded.',
 	);
 
 	//get average essay score
 	const gradesArr = [0];
-	essaysGraded.forEach((f) =>
-		gradesArr.push(parseFloat(f?.metadata?.currentGrade)),
-	);
-	const avgGrade = gradesArr?.reduce((a, b) => a + b) / gradesArr?.length;
+	essaysGraded.forEach((f) => gradesArr.push(parseFloat(f?.currentGrade)));
+	const avgGrade =
+		gradesArr?.reduce((a, b) => a + b) / (gradesArr?.length - 1);
+	const gradeFunc = () => {
+		if (gradesArr.length === 2) {
+			return gradesArr[1].toFixed(2);
+		} else if (gradesArr.length === 1) {
+			return 'No scores yet.';
+		} else {
+			return avgGrade.toFixed(2);
+		}
+	};
 
 	//get average time spent grading each essay
 	const times = [0];
-	essaysGraded.forEach((f) =>
-		times.push(parseFloat(f?.metadata?.timeSpentGrading)),
-	);
+	essaysGraded.forEach((f) => times.push(parseFloat(f?.timeSpentGrading)));
 	const avgTime = times?.reduce((a, b) => a + b) / times?.length;
 
 	//get last graded essay
 	let updates = [0];
-	essaysGraded.forEach((f) => updates.push(f?.metadata?.lastUpdated));
+	essaysGraded.forEach((f) => updates.push(f?.lastUpdated));
 	const mostRecentDate = updates.sort((a, b) => new Date(b) - new Date(a))[0];
-	const recentEssay = thisUsersFiles.filter(
-		(f) => f?.metadata?.lastUpdated === mostRecentDate,
+	const recentEssay = thisUsersFileData.filter(
+		(f) => f?.lastUpdated === mostRecentDate,
 	);
+
+	console.log(thisUsersFiles);
+	console.log(thisUsersFileData);
 
 	return (
 		<div className='page-container'>
@@ -66,15 +82,11 @@ function Dashboard({ rubrics, files }) {
 							<div className='stat-container'>
 								<p className='dash-stat'>
 									{recentEssay[0] === undefined
-										? 'No grades yet'
-										: recentEssay[0]?.metadata?.student !==
-										  ''
-										? recentEssay[0]?.filename +
-										  ' (' +
-										  recentEssay[0]?.metadata?.student +
-										  ')'
+										? 'No grades yet.'
 										: recentEssay[0]?.filename +
-										  '\n(no student entered)'}
+										  ' (' +
+										  recentEssay[0]?.student +
+										  ')'}
 								</p>
 							</div>
 						</div>
@@ -89,9 +101,7 @@ function Dashboard({ rubrics, files }) {
 						<div id='dash-col' className='col-sm'>
 							<h5>Average Essay Score</h5>
 							<div className='stat-container'>
-								<p className='dash-stat'>
-									{avgGrade.toFixed(2)}
-								</p>
+								<p className='dash-stat'>{gradeFunc()}</p>
 							</div>
 						</div>
 					</div>
