d6ee3e620f39426e73e1a7088a80f421cafa8cf2
Marissa Gvozdenovich <66572562+mgvoz@users.noreply.github.com>
2021-05-10 16:31:30 -0400

    final edits

--- server/routes/files.js
+++ server/routes/files.js
@@ -6,7 +6,6 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const fileRouter = express.Router();
 var ObjectId = require('mongodb').ObjectID;
-const fs = require('fs');
 
 export const fileRoutes = (upload) => {
 	const url = CONNECTION_URL;
@@ -27,9 +26,15 @@ export const fileRoutes = (upload) => {
         POST: Upload a single image/file to Image collection
     */
 
-	fileRouter.post('/upload', upload.single('inputFile'), (req, res, next) => {
-		res.redirect('http://localhost:3000/upload');
-	});
+	fileRouter.post(
+		'/upload/:userID',
+		upload.single('inputFile'),
+		(req, res) => {
+			res.redirect(
+				`https://essay-flow.netlify.app/upload/${req.params.userID}`,
+			);
+		},
+	);
 
 	/*
         GET: Fetches all the files in the uploads collection
@@ -61,38 +66,6 @@ export const fileRoutes = (upload) => {
 			console.log(file);
 			res.status(200).json(file);
 		});
-	});
-
-	/*
-        GET: Update a particular file's metadata by id
-    */
-	fileRouter.get('/student/:id', (req, res) => {
-		var filter = { _id: ObjectId(req.params.id) };
-		var newvalues = { $set: { 'metadata.student': req.cookies.student } };
-		try {
-			gfs.files.updateOne(filter, newvalues, { new: true });
-			res.redirect('http://localhost:3000/library/*');
-		} catch (err) {
-			res.status(404).json({ message: err.message });
-		}
-	});
-
-	fileRouter.get('/grade/:id', (req, res) => {
-		var filter = { _id: ObjectId(req.params.id) };
-		var newvalues = {
-			$set: {
-				'metadata.currentGrade': req.cookies.currentGrade,
-				'metadata.notes': req.cookies.notes,
-				'metadata.timeSpentGrading': req.cookies.timeSpentGrading,
-				'metadata.lastUpdated': req.cookies.lastUpdated,
-			},
-		};
-		try {
-			gfs.files.updateOne(filter, newvalues, { new: true });
-			res.redirect('http://localhost:3000/library/*');
-		} catch (err) {
-			res.status(404).json({ message: err.message });
-		}
 	});
 
 	/* 
@@ -129,7 +102,7 @@ export const fileRoutes = (upload) => {
 					return res.status(404).json({ err: err });
 				}
 
-				res.redirect('http://localhost:3000/library/*');
+				res.redirect('https://essay-flow.netlify.app/library/*');
 			},
 		);
 	});
