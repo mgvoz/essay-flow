9a819c6d685d639841802f3145321a0cc36aff7d
Marissa Gvozdenovich <66572562+mgvoz@users.noreply.github.com>
2021-07-20 14:23:38 -0400

    Update Home.js
    added functionality to direct users to computer instead of mobile

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../images/twitter_header_photo_2.png';
 
 function Home() {
+	const [width, setWidth] = useState(0);
+
+	useEffect(() => {
+		setTimeout(() => setWidth(window.innerWidth), 200);
+	}, []);
+
 	return (
-		<div className='page-container'>
-			<div className='login-page'>
-				<h1 className='main-title'>Welcome to Essay Flow!</h1>
-				<p className='main-description'>
-					Essay Flow is built for teachers, by a teacher. Never before
-					has your essay-grading workflow been easier. Please sign up
-					or sign in to continue.
-				</p>
-			</div>
-			<img className='home-logo' src={logo} alt='logo' />
-		</div>
+		<>
+			{width >= 500 ? (
+				<div className='page-container'>
+					<div className='login-page'>
+						<h1 className='main-title'>Welcome to Essay Flow!</h1>
+						<p className='main-description'>
+							Essay Flow is built for teachers, by a teacher.
+							Never before has your essay-grading workflow been
+							easier. Please sign up or sign in to continue.
+						</p>
+					</div>
+					<img className='home-logo' src={logo} alt='logo' />
+				</div>
+			) : (
+				<div className='page-container'>
+					<div className='login-page'>
+						<h1 className='main-title'>Welcome to Essay Flow!</h1>
+						<p className='main-description'>
+							ATTENTION: Essay Flow is meant to be used on laptop
+							or desktop computers, rather than mobile. Please
+							navigate to this page on a laptop or desktop to
+							continue.
+						</p>
+					</div>
+					<img className='home-logo' src={logo} alt='logo' />
+				</div>
+			)}
+		</>
 	);
 }
 
 export default Home;
+
